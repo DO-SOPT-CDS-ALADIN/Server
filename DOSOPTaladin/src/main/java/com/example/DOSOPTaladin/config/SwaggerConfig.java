663ec16a08a46f0a0336cdc0aab32c40caac09a8
ziiyouth <ziy913@naver.com>
2023-11-18 18:36:22 +0900

    [setting]: 스웨거 연동

---
+++ DOSOPTaladin/src/main/java/com/example/DOSOPTaladin/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.DOSOPTaladin.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        Info info = new Info()
+                .title("DO-SOPT-CDS Team Web1 API Document")
+                .version("v0.1")
+                .description("SOPT 33기 합동 세미나 Web 1팀 API 명세서입니다.");
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .components(new Components())
+                .info(info);
+    }
+}
