99e5c9f6c014e6d9c1bd772811b0ce65657c5b65
hellozo0 <ahju0011@gmail.com>
2023-11-18 17:23:15 +0900

    [SETTING] project setting

---
+++ DOSOPTaladin/src/test/java/com/example/DOSOPTaladin/DosopTaladinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DOSOPTaladin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosopTaladinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
