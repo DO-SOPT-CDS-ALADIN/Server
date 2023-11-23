e135f9e49931b159a46b8bc0106da6a7b8497987
hellozo0 <ahju0011@gmail.com>
2023-11-23 23:12:39 +0900

    [FEAT] Cart Repository 생성

---
+++ DOSOPTaladin/src/main/java/com/example/DOSOPTaladin/repository/CartJpaRepository.java
@@ -0,0 +1,9 @@
+package com.example.DOSOPTaladin.repository;
+
+import com.example.DOSOPTaladin.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartJpaRepository extends JpaRepository<Cart, Long> {
+}
