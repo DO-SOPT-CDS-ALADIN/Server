68bb8f5d7138efe49f0217bc7ad0f35deeeb1535
hellozo0 <ahju0011@gmail.com>
2023-11-24 13:51:46 +0900

    [FEAT] HeartRepository 오타 수정...

--- DOSOPTaladin/src/main/java/com/example/DOSOPTaladin/service/BestService.java
+++ DOSOPTaladin/src/main/java/com/example/DOSOPTaladin/service/BestService.java
@@ -1,19 +1,15 @@
 package com.example.DOSOPTaladin.service;
 
 import com.example.DOSOPTaladin.domain.Book;
-import com.example.DOSOPTaladin.domain.Heart;
 import com.example.DOSOPTaladin.dto.response.best.BestBookResponse;
-import com.example.DOSOPTaladin.dto.response.main.EditorChoiceResponse;
 import com.example.DOSOPTaladin.repository.BookJpaRepository;
-import com.example.DOSOPTaladin.repository.HeartJpaRespository;
+import com.example.DOSOPTaladin.repository.HeartJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +17,7 @@ import java.util.stream.Collectors;
 public class BestService {
 
     private final BookJpaRepository bookJpaRepository;
-    private final HeartJpaRespository heartJpaRespository;
+    private final HeartJpaRepository heartJpaRepository;
 
     public List<BestBookResponse> getBestBookList() {
         List<Book> books = bookJpaRepository.findAll();
@@ -30,7 +26,7 @@ public class BestService {
 
         for( Book book : books){
             BestBookResponse bestBookResponse = new BestBookResponse();
-            bestBookResponse = new BestBookResponse(book, heartJpaRespository.existsByBook_Id(book.getId()));
+            bestBookResponse = new BestBookResponse(book, heartJpaRepository.existsByBook_Id(book.getId()));
 
             bestBookResponseList.add(bestBookResponse);
         }
